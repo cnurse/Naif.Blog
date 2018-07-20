5c29d8faa90d3c502c8996fa8129012cf1fab0f0
Charles Nurse <charlesnurse@gmail.com>
2018-07-20 09:18:12 -0700

    Added initial Admin files and included sample blogs.json file

---
+++ src/Naif.Blog/ViewModels/BlogViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Naif.Blog.Models;
+
+namespace Naif.Blog.ViewModels
+{
+    public class BlogViewModel
+    {
+        public Naif.Blog.Models.Blog Blog { get; set; }
+        
+        public int Page { get; set; }
+        
+        public Post Post { get; set; }
+
+        public IEnumerable<Post> Posts { get; set; }
+    }
+}
